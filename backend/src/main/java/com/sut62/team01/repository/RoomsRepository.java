23e1e78a57da728c0e9e4b5409bc0debc0e3c2ed
usernamefrance060 <pakorn_france98@hotmail.com>
2020-01-08 00:55:55 +0700

    Repository

---
+++ backend/src/main/java/com/sut62/team01/repository/RoomsRepository.java
@@ -0,0 +1,11 @@
+package com.sut62.team01.repository;
+
+import com.sut62.team01.entity.Rooms;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface RoomsRepository extends JpaRepository<Rooms, Long> {
+	Rooms findById(long id);
+}
