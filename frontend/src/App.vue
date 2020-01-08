b3cea772b7dafbcf7fb657752479e1c875087583
pontep <pontep.taveeshup@gmail.com>
2020-01-08 23:08:01 +0700

    แยก staff , student item

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -6,57 +6,112 @@
       app
     >
       <template v-if="isSignin">
-        <v-list dense>
-          <template v-for="item in items">
-            <v-row v-if="item.heading" :key="item.heading" align="center">
-              <v-col cols="6">
-                <v-subheader v-if="item.heading">
-                  {{ item.heading }}
-                </v-subheader>
-              </v-col>
-              <v-col cols="6" class="text-center">
-                <a href="#!" class="body-2 black--text">EDIT</a>
-              </v-col>
-            </v-row>
-            <v-list-group
-              v-else-if="item.children"
-              :key="item.text"
-              v-model="item.model"
-              :prepend-icon="item.model ? item.icon : item['icon-alt']"
-              append-icon=""
-            >
-              <template v-slot:activator>
-                <v-list-item>
+        <template v-if="user.studentId">
+          <v-list dense>
+            <template v-for="item in items">
+              <v-row v-if="item.heading" :key="item.heading" align="center">
+                <v-col cols="6">
+                  <v-subheader v-if="item.heading">
+                    {{ item.heading }}
+                  </v-subheader>
+                </v-col>
+                <v-col cols="6" class="text-center">
+                  <a href="#!" class="body-2 black--text">EDIT</a>
+                </v-col>
+              </v-row>
+              <v-list-group
+                v-else-if="item.children"
+                :key="item.text"
+                v-model="item.model"
+                :prepend-icon="item.model ? item.icon : item['icon-alt']"
+                append-icon=""
+              >
+                <template v-slot:activator>
+                  <v-list-item>
+                    <v-list-item-content>
+                      <v-list-item-title>
+                        {{ item.text }}
+                      </v-list-item-title>
+                    </v-list-item-content>
+                  </v-list-item>
+                </template>
+                <v-list-item v-for="(child, i) in item.children" :key="i" link>
+                  <v-list-item-action v-if="child.icon">
+                    <v-icon>{{ child.icon }}</v-icon>
+                  </v-list-item-action>
                   <v-list-item-content>
                     <v-list-item-title>
-                      {{ item.text }}
+                      {{ child.text }}
                     </v-list-item-title>
                   </v-list-item-content>
                 </v-list-item>
-              </template>
-              <v-list-item v-for="(child, i) in item.children" :key="i" link>
-                <v-list-item-action v-if="child.icon">
-                  <v-icon>{{ child.icon }}</v-icon>
+              </v-list-group>
+              <v-list-item v-else :key="item.text" link @click="item.click">
+                <v-list-item-action>
+                  <v-icon>{{ item.icon }}</v-icon>
                 </v-list-item-action>
                 <v-list-item-content>
                   <v-list-item-title>
-                    {{ child.text }}
+                    {{ item.text }}
                   </v-list-item-title>
                 </v-list-item-content>
               </v-list-item>
-            </v-list-group>
-            <v-list-item v-else :key="item.text" link @click="item.click">
-              <v-list-item-action>
-                <v-icon>{{ item.icon }}</v-icon>
-              </v-list-item-action>
-              <v-list-item-content>
-                <v-list-item-title>
-                  {{ item.text }}
-                </v-list-item-title>
-              </v-list-item-content>
-            </v-list-item>
-          </template>
-        </v-list>
+            </template>
+          </v-list>
+        </template>
+        <template v-else>
+          <v-list dense>
+            <template v-for="item in staffItems">
+              <v-row v-if="item.heading" :key="item.heading" align="center">
+                <v-col cols="6">
+                  <v-subheader v-if="item.heading">
+                    {{ item.heading }}
+                  </v-subheader>
+                </v-col>
+                <v-col cols="6" class="text-center">
+                  <a href="#!" class="body-2 black--text">EDIT</a>
+                </v-col>
+              </v-row>
+              <v-list-group
+                v-else-if="item.children"
+                :key="item.text"
+                v-model="item.model"
+                :prepend-icon="item.model ? item.icon : item['icon-alt']"
+                append-icon=""
+              >
+                <template v-slot:activator>
+                  <v-list-item>
+                    <v-list-item-content>
+                      <v-list-item-title>
+                        {{ item.text }}
+                      </v-list-item-title>
+                    </v-list-item-content>
+                  </v-list-item>
+                </template>
+                <v-list-item v-for="(child, i) in item.children" :key="i" link>
+                  <v-list-item-action v-if="child.icon">
+                    <v-icon>{{ child.icon }}</v-icon>
+                  </v-list-item-action>
+                  <v-list-item-content>
+                    <v-list-item-title>
+                      {{ child.text }}
+                    </v-list-item-title>
+                  </v-list-item-content>
+                </v-list-item>
+              </v-list-group>
+              <v-list-item v-else :key="item.text" link @click="item.click">
+                <v-list-item-action>
+                  <v-icon>{{ item.icon }}</v-icon>
+                </v-list-item-action>
+                <v-list-item-content>
+                  <v-list-item-title>
+                    {{ item.text }}
+                  </v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+            </template>
+          </v-list>
+        </template>
       </template>
       <template v-else>
         <v-list dense>
@@ -157,10 +212,35 @@ export default {
   data() {
     return {
       isSignin: undefined,
-      isStudent: undefined,
       user: {},
       dialog: false,
       drawer: null,
+      staffItems: [
+        {
+          icon: "mdi-home",
+          role: "all",
+          text: "หน้าหลัก",
+          click: () => {
+            this.$router.push("/home");
+          }
+        },
+        {
+          icon: "mdi-contacts",
+          role: "staff",
+          text: "ลงทะเบียนยานพาหนะ",
+          click: () => {
+            this.$router.push("/borrowedBike");
+          }
+        },
+        {
+          icon: "mdi-contacts",
+          role: "staff",
+          text: "ลงทะเบียนเครื่องใช้ไฟฟ้า",
+          click: () => {
+            this.$router.push("/borrowedBike");
+          }
+        }
+      ],
       items: [
         {
           icon: "mdi-home",
@@ -182,22 +262,6 @@ export default {
           icon: "mdi-contacts",
           role: "student",
           text: "ยืมจักรยาน",
-          click: () => {
-            this.$router.push("/borrowedBike");
-          }
-        },
-        {
-          icon: "mdi-contacts",
-          role: "staff",
-          text: "ลงทะเบียนเครื่องใช้ไฟฟ้า",
-          click: () => {
-            this.$router.push("/borrowedBike");
-          }
-        },
-        {
-          icon: "mdi-contacts",
-          role: "staff",
-          text: "ลงทะเบียนยานพาหนะ",
           click: () => {
             this.$router.push("/borrowedBike");
           }
