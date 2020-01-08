94af6670efe001543024cf25dbae4097c3743ab7
pontep <pontep.taveeshup@gmail.com>
2020-01-08 11:02:23 +0700

    hide data.sql

---
+++ backend/src/main/resources/#data.sql
@@ -0,0 +1,22 @@
+-- INSERT INTO bike_type
+-- VALUES
+--     ('1', '2 wheels');
+-- INSERT INTO bike_type
+-- VALUES
+--     ('2', '3 wheels');
+-- INSERT INTO bike_type
+-- VALUES
+--     ('3', '4 wheels');
+
+-- INSERT INTO student
+--     (student_id,username,password,name,bachelor)
+-- VALUES
+--     ('1', 'pontep', 'asdasd', 'Pontep Thaweesup', 'Computer Engineering');
+-- INSERT INTO student
+--     (student_id,username,password,name,bachelor)
+-- VALUES
+--     ('2', 'kettiya', '123456', 'Kettiya Muangkeaw', 'Aeroneutical Engineering');
+-- INSERT INTO student
+--     (student_id,username,password,name,bachelor)
+-- VALUES
+--     ('3', 'admin', 'admin', 'Administrator', 'Teacher');
