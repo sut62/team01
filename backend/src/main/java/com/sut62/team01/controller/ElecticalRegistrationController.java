9897ff8216804e86c5596fcbd77767d86516739a
unknown <pummarinB6003296@gmail.com>
2020-01-08 13:55:30 +0700

    ทำpostmapping แบบใหม่

--- backend/src/main/java/com/sut62/team01/controller/ElecticalRegistrationController.java
+++ backend/src/main/java/com/sut62/team01/controller/ElecticalRegistrationController.java
@@ -2,6 +2,7 @@ package com.sut62.team01.controller;
 
 import java.util.Collection;
 import java.util.Date;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import com.sut62.team01.entity.ElectricType;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -54,5 +56,21 @@ public class ElecticalRegistrationController {
         newElectricalRegistration.setRoomBooking(roomBooking);
         newElectricalRegistration.setElectricType(electricType);
         return electricalregitrationRepository.save(newElectricalRegistration);
-    };
+    }
+
+    @PostMapping("/electricRegister")
+    public ElectricalRegistration newElectricalRegistration(@RequestBody NewElectricalRegistrationPayload electric) {
+        ElectricalRegistration electricalRegistration = new ElectricalRegistration();
+
+        Optional<Staff> staff = staffRepository.findById(electric.getStaffId());
+        Optional<RoomBooking> roomBooking = roombookingRepository.findById(electric.getRoomBookingId());
+        Optional<ElectricType> electricType = electrictypeRepository.findById(electric.getElectricTypeId());
+
+        electricalRegistration.setElectricalRegistrationdate(new Date());
+        electricalRegistration.setStaff(staff.get());
+        electricalRegistration.setRoomBooking(roomBooking.get());
+        electricalRegistration.setElectricType(electricType.get());
+        electricalRegistration.setDetails(electric.getDetails());
+        return electricalregitrationRepository.save(electricalRegistration);
+    }
 }
