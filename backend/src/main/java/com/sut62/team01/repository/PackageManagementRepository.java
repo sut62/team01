780e13cd0b12a22d4699ba3effc4d27d2c772517
Chiraphon Worasuk <apple10144@hotmail.com>
2020-01-08 21:23:25 +0700

    add PackageManagementRepository - close #69

---
+++ backend/src/main/java/com/sut62/team01/repository/PackageManagementRepository.java
@@ -0,0 +1,14 @@
+package com.sut62.team01.repository;
+
+import com.sut62.team01.entity.PackageManagement;
+import com.sut62.team01.entity.RoomBooking;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface PackageManagementRepository extends JpaRepository<PackageManagement, Long> {
+    PackageManagement findById(long id);
+
+    PackageManagement findByRoomBooking(RoomBooking roomBooking);
+}
