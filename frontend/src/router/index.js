3a64333e53d5e53865815d53be58a3f35772fbe1
pontep <pontep.taveeshup@gmail.com>
2020-01-08 22:10:43 +0700

    fix confix fronk

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -39,11 +39,14 @@ const routes = [
       import(/* webpackChunkName: "about" */ "../views/Profile.vue")
   },
   {
+    path: "/staff/signin",
+    name: "staffsignin",
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/StaffSignin.vue")
+  },
+  {
     path: "/roombooking",
     name: "roombooking",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/RoomBooking.vue")
   }
@@ -55,7 +58,7 @@ const router = new VueRouter({
   routes
 });
 router.beforeEach((to, from, next) => {
-  const publicPages = ["/signin", "/", "/register"];
+  const publicPages = ["/signin", "/", "/register", "/staff/signin"];
   const authRequired = !publicPages.includes(to.path);
   const loggedIn = localStorage.getItem("user");
 
---
+++ frontend/src/views/StaffSignin.vue
@@ -0,0 +1,76 @@
+<template>
+  <v-row align="center" justify="center">
+    <v-col cols="12" sm="8" md="6">
+      <v-card class="elevation-12">
+        <v-toolbar color="amber" dark flat>
+          <v-toolbar-title>STAFF Sign in</v-toolbar-title>
+          <v-spacer />
+        </v-toolbar>
+        <v-card-text>
+          <v-form>
+            <v-text-field
+              label="Login"
+              name="login"
+              prepend-icon="mdi-account"
+              type="text"
+              v-model="username"
+            />
+
+            <v-text-field
+              id="password"
+              label="Password"
+              name="password"
+              prepend-icon="mdi-lock"
+              type="password"
+              v-model="password"
+            />
+          </v-form>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer />
+          <v-btn color="amber" @click="signin">Login</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+import api from "../Api.js";
+
+export default {
+  name: "sigin",
+  data() {
+    return {
+      username: undefined,
+      password: undefined
+    };
+  },
+  methods: {
+    signin() {
+      let user = {
+        username: this.username,
+        password: this.password
+      };
+      api
+        .post("/auth/login", JSON.stringify(user))
+        .then(res => {
+          let user = res.data;
+          alert("Signin successfully..." + user.username);
+          localStorage.setItem("user", JSON.stringify(user));
+          this.$router.go("/home");
+        })
+        .catch(e => {
+          alert(e);
+        });
+    }
+  },
+  mounted() {
+    if (localStorage.getItem("user")) {
+      this.$router.push("/home");
+    }
+  }
+};
+</script>
+
+<style></style>
