81e1ddc4a0ffbd667a7aba5ad7d74996a3896ebe
pontep <pontep.taveeshup@gmail.com>
2020-01-22 00:48:47 +0700

    old

--- frontend/src/views/ApproveBorrowedBike.vue
+++ frontend/src/views/ApproveBorrowedBike.vue
@@ -9,25 +9,35 @@
           <v-spacer />
         </v-toolbar>
         <v-card-text>
-          <template v-for="bb in borrowedBikes">
-            <v-card :key="bb.id">
-              <v-toolbar
-                color="amber darken-3"
-                dark
-              >{{ bb.id }} | {{ bb.roomBooking.student.fullName }} | {{ bb.dateType.type }} | {{ bb.bikeType.type }}</v-toolbar>
-              <v-card-title primary-title></v-card-title>
-              <v-card-text>
-                <v-select
-                  v-model="selectedBike"
-                  :items="getBikes(bb.bikeType.id)"
-                  item-text="name"
-                  label="เลือกจักรยาน"
-                ></v-select>
-                {{ selectedBike }} asd
-              </v-card-text>
-              <v-card-actions>
-                <v-btn color="success" @click="test">อนุมัติ</v-btn>
-              </v-card-actions>
+          <template v-for="(bb) in borrowedBikes">
+            <v-card :key="bb.i" style="margin: 20px">
+              <v-toolbar color="white darken-4" light>
+                <v-icon>mdi-key</v-icon>
+                {{ bb.id }}
+                <!-- <v-btn color="blue" text></v-btn> -->
+                <v-icon>mdi-account</v-icon>
+                {{ bb.roomBooking.student.fullName }}
+                |
+                <v-icon>mdi-calendar-today</v-icon>
+                {{ bb.dateType.type }} |
+                <v-icon>mdi-bicycle</v-icon>
+                {{ bb.bikeType.type }}
+                <v-spacer></v-spacer>
+                <v-btn color="amber darken-3" dark @click="handleDialog(bb)">เลือกคำร้อง</v-btn>
+              </v-toolbar>
+              <v-divider></v-divider>
+              <v-dialog v-model="dialog" width="500">
+                <v-card>
+                  <v-card-title class="headline grey lighten-2" primary-title>WTF</v-card-title>
+                  <v-card-text>{{ dialog_fullName }}</v-card-text>
+                  <v-divider></v-divider>
+                  <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn color="success" text @click="handleApproveBorrowedBike">อนุมัติ</v-btn>
+                    <v-btn color="error" text @click="dialog = false">ยกเลิก</v-btn>
+                  </v-card-actions>
+                </v-card>
+              </v-dialog>
             </v-card>
           </template>
         </v-card-text>
@@ -46,23 +56,21 @@ export default {
   name: "approveBorrowedBike",
   data() {
     return {
+      dialog_fullName: null,
+      dialog: false,
       selectedBike: null,
       bikes: [],
       search: null,
-      borrowedBikes: [],
-      headers: [
-        {
-          text: "BorrowedBikeId",
-          align: "left",
-          sortable: false,
-          value: "id"
-        },
-        { text: "Student name", value: "roomBooking.student.fullName" },
-        { text: "Bike type", value: "bikeType.type" }
-      ]
+      borrowedBikes: []
     };
   },
   methods: {
+    handleApproveBorrowedBike() {},
+    handleDialog(bb) {
+      this.dialog = true;
+      this.dialog_fullName = bb.roomBooking.student.fullName;
+      console.log(JSON.parse(JSON.stringify(bb)));
+    },
     test() {
       return [{ name: "1234" }, { name: "456" }];
     },
@@ -76,12 +84,10 @@ export default {
         .post("/api/bike", body)
         .then(res => {
           this.bikes = res.data;
-          // bikes = res.data;
           console.log(JSON.parse(JSON.stringify(res.data)));
         })
         .catch(e => {
           console.log(e);
-          return null;
         });
     },
     getAllBorrowedBikes() {
@@ -89,8 +95,6 @@ export default {
         .get("/api/borrowedbikes")
         .then(res => {
           this.borrowedBikes = res.data;
-          console.log("borrowedBikes: ");
-          console.log(JSON.parse(JSON.stringify(this.borrowedBikes)));
         })
         .catch(e => {
           console.log(e);
