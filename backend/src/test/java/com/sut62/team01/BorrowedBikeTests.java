567a27cb334ab6595b190bf86839fed96e1cb6cd
dincracker <pontep.taveeshup@gmail.com>
2020-01-25 02:00:31 +0700

    close #126 - OK

--- backend/src/test/java/com/sut62/team01/BorrowedBikeTests.java
+++ backend/src/test/java/com/sut62/team01/BorrowedBikeTests.java
@@ -85,6 +85,7 @@ public class BorrowedBikeTests {
         assertEquals(borrowedBike, found.get());
     }
 
+//    OK
     @Test
     void b6000783_testPutDataApproveBorrowedBikeOK() {
         // จำลองข้อมูลที่จำเป็นในการบันทึก BorrowedBike
