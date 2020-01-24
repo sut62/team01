a6bbc1cf1b4a32225eabba63e75aa415e2b19b06
Chiraphon Worasuk <apple10144@hotmail.com>
2020-01-24 16:20:38 +0700

    add UI ConfirmPackage - close #114

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -94,6 +94,15 @@ const routes = [
     name: "showElectric",
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/ShowElectric.vue")
+  },
+  {
+    path: "/staff/confirmPackage",
+    name: "confirmPackage",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/ConfirmPackage.vue")
   }
 ];
 
