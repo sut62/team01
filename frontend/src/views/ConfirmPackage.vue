cd2139de5d135214837624902ed9ae3ad8f04d46
Chiraphon Worasuk <apple10144@hotmail.com>
2020-01-25 00:10:42 +0700

    fix katalon PackageM - close #144

--- frontend/src/views/ConfirmPackage.vue
+++ frontend/src/views/ConfirmPackage.vue
@@ -1,7 +1,11 @@
 <template>
   <v-container>
-      <v-alert type="success" dismissible v-model="alertSuccess">บันทึกข้อมูลการ Confirm เรียบร้อย</v-alert>
-      <v-alert type="error" dismissible v-model="alertFailed">กรุณาเลือกข้อมูลให้ครบทุกช่อง!</v-alert>
+    <v-row align="center" justify="center">
+      <v-col cols="12" sm="8" md="6">
+        <v-alert type="success" dismissible v-model="alertSuccess">บันทึกข้อมูลการ Confirm เรียบร้อย</v-alert>
+        <v-alert type="yellow darken-2" dismissible v-model="alertFailed">ไม่ได้ทำการบันทึกข้อมูล</v-alert>
+      </v-col>
+    </v-row>
     <v-layout text-center wrap>
       <v-flex xs3 sm6 md9 lg12>
         <br />
@@ -13,13 +17,28 @@
           class="elevation-1"
         >
           <template v-slot:item.confirm="{ item }">
-            <v-btn class="btn btn-danger mr-1" color="warning" @click="save(item)">confirm</v-btn>
+            <div class="btn btn-danger mr-1">
+              <v-bottom-sheet v-model="sheet" persistent>
+                <template v-slot:activator="{ on }">
+                  <v-btn color="warning" dark v-on="on">confirm</v-btn>
+                </template>
+                <v-row align="right" justify="center">
+                  <v-sheet class="text-center" height="150px" width="600px">
+                    <v-btn class="ma-2" flat color="error" @click="sheet = !sheet; cancle()">cancel</v-btn>
+                    <v-btn class="ma-2" flat color="success" @click="save(item)">save</v-btn>
+                    <div class="py-3">กรุณากดปุ่ม save เพื่อยืนยันการรับพัสดุ</div>
+                  </v-sheet>
+                </v-row>
+              </v-bottom-sheet>
+            </div>
           </template>
         </v-data-table>
       </v-flex>
     </v-layout>
   </v-container>
 </template>
+
+        
 
 <script>
 import api from "../Api.js";
@@ -31,6 +50,7 @@ export default {
   },
   data() {
     return {
+      sheet: false,
       alertSuccess: false,
       alertFailed: false,
       selectedPackage: null,
@@ -67,21 +87,18 @@ export default {
           api
             .get("/api/confirmPackage")
             .then(res => {
-              console.log("ConfirmPackage loaded.")
+              console.log("ConfirmPackage loaded.");
               confirmPackage = res.data;
               console.log(confirmPackage);
               for (var i = 0; i < temp.length; i++) {
-                for (var j = 0; j < confirmPackage.length ; j++) {
-                  if (
-                    temp[i].id ==
-                    confirmPackage[j].packageManagement.id
-                  ) {
+                for (var j = 0; j < confirmPackage.length; j++) {
+                  if (temp[i].id == confirmPackage[j].packageManagement.id) {
                     temp.splice(i, 1);
                   }
                 }
-                console.log("temp:")
-                console.log(JSON.parse(JSON.stringify(temp)))
-                 this.PackageManagements = temp;
+                console.log("temp:");
+                console.log(JSON.parse(JSON.stringify(temp)));
+                this.PackageManagements = temp;
               }
             })
             .catch(e => {
@@ -95,6 +112,10 @@ export default {
     clearAlert() {
       this.alertSuccess = false;
       this.alertFailed = false;
+    },
+    cancle() {
+      this.clearAlert();
+      this.alertFailed = true;
     },
     save(pm) {
       // Debug
