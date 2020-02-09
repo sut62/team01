02df10c0e222b462d905c7fa6f7e522672233de4
Chiraphon Worasuk <apple10144@hotmail.com>
2020-02-09 15:19:51 +0700

    add test NotNull in ConfirmP - close #198

--- backend/src/test/java/com/sut62/team01/ConfirmPackageTest.java
+++ backend/src/test/java/com/sut62/team01/ConfirmPackageTest.java
@@ -120,4 +120,70 @@ public class ConfirmPackageTest {
         assertEquals("packageManagement", result.iterator().next().getPropertyPath().toString());
 
     }
+
+    @Test
+    void b6018474_testConfirmDateMustNotBeNull() {
+        ConfirmPackage confirmPackage = new ConfirmPackage();
+
+        // necessary for roombooking
+        Students students = new Students("Pontep Thaweesup", "B6000783", "วิศวกรรมศาสตร์", "pontep", "1234");
+        students = studentsRepository.saveAndFlush(students);
+        Rooms rooms = new Rooms("7133");
+        rooms = roomsRepository.saveAndFlush(rooms);
+        Bed bed = new Bed("What's branch?");
+        bed = bedRepository.saveAndFlush(bed);
+        RoomBooking roomBooking = new RoomBooking(students, rooms, bed, "pakorn@hotmail.com");
+        roomBooking = roomBookingRepository.saveAndFlush(roomBooking);
+        // necessary for PackageManagement
+        PackageType packageType = new PackageType("sth");
+        packageType = packageTypeRepository.saveAndFlush(packageType);
+        Staff staff = new Staff("TestStaff", "test", "1234");
+        staff = staffRepository.saveAndFlush(staff);
+        PackageManagement packageManagement = new PackageManagement(roomBooking, packageType, staff, "details");
+        packageManagement = packageManagementRepository.saveAndFlush(packageManagement);
+
+        confirmPackage.setPackageManagement(packageManagement);
+        confirmPackage.setConfirmDate(null);
+        confirmPackage.setStaff(staff);
+
+        Set<ConstraintViolation<ConfirmPackage>> result = validator.validate(confirmPackage);
+
+        assertEquals(1, result.size());
+        assertEquals("must not be null", result.iterator().next().getMessage());
+        assertEquals("confirmDate", result.iterator().next().getPropertyPath().toString());
+
+    }
+
+    @Test
+    void b6018474_testStaffMustNotBeNull() {
+        ConfirmPackage confirmPackage = new ConfirmPackage();
+
+        // necessary for roombooking
+        Students students = new Students("Pontep Thaweesup", "B6000783", "วิศวกรรมศาสตร์", "pontep", "1234");
+        students = studentsRepository.saveAndFlush(students);
+        Rooms rooms = new Rooms("7133");
+        rooms = roomsRepository.saveAndFlush(rooms);
+        Bed bed = new Bed("What's branch?");
+        bed = bedRepository.saveAndFlush(bed);
+        RoomBooking roomBooking = new RoomBooking(students, rooms, bed, "pakorn@hotmail.com");
+        roomBooking = roomBookingRepository.saveAndFlush(roomBooking);
+        // necessary for PackageManagement
+        PackageType packageType = new PackageType("sth");
+        packageType = packageTypeRepository.saveAndFlush(packageType);
+        Staff staff = new Staff("TestStaff", "test", "1234");
+        staff = staffRepository.saveAndFlush(staff);
+        PackageManagement packageManagement = new PackageManagement(roomBooking, packageType, staff, "details");
+        packageManagement = packageManagementRepository.saveAndFlush(packageManagement);
+
+        confirmPackage.setPackageManagement(packageManagement);
+        confirmPackage.setConfirmDate(new Date());
+        confirmPackage.setStaff(null);
+
+        Set<ConstraintViolation<ConfirmPackage>> result = validator.validate(confirmPackage);
+
+        assertEquals(1, result.size());
+        assertEquals("must not be null", result.iterator().next().getMessage());
+        assertEquals("staff", result.iterator().next().getPropertyPath().toString());
+
+    }
 }
